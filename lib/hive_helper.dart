b505a2708cb62bf24ea3a6217d64bdc598e52f9f
Mohamed Haddaoui <mhaddaou@e3r4p2.1337.ma>
2025-06-19 21:09:04 +0100

    up

--- lib/hive_helper.dart
+++ lib/hive_helper.dart
@@ -1,14 +1,25 @@
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 
 class HiveHelper {
   static const String _boxName = 'swifty-companion';
+
+  static Future<String> getAppDirectory() async{
+    if(kIsWeb){
+      return '/';
+    }else{
+      final appDocumentDir = await getApplicationDocumentsDirectory();
+      return appDocumentDir.path;
+    }
+  }
   
   // Initialize Hive
   static Future<void> init() async {
-    final appDocumentDir = await getApplicationDocumentsDirectory();
-      Hive.init(appDocumentDir.path);
+    
+    final appDocumentDir = await getAppDirectory();
+      Hive.init(appDocumentDir);
     
     await Hive.openBox(_boxName);
   }
